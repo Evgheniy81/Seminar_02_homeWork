61ca1897e69b8e8b887aa1f0b6af4aa3d444bbc0
Jeka_A <antimur4ik@yandex.ru>
2023-06-20 15:59:34 +0300

    Задача с выходными и буднями

---
+++ Task_15/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+///////////////
+
+Console.Write($"Enter the number => ");
+int n = Convert.ToInt32(Console.ReadLine());
+Dictionary<int, string> map = new Dictionary<int, string>()
+{
+    {1, "Monday"},
+    {2, "Tuesday"},
+    {3, "Wednesday"},
+    {4, "Thursday"},
+    {5, "Friday"},
+    {6, "Saturday"},
+    {7, "Sunday"}
+};
+if (n > 0 & n < 6)
+{
+    Console.WriteLine($"{map[n]} - рабочий день");
+    
+}
+else 
+{
+    Console.WriteLine($"{map[n]} - выходной день");
+}
+
+
+//////////////// с командой "switch" у меня пока только так получилось
+/*
+Console.Write($"Enter the number => ");
+int n = Convert.ToInt32(Console.ReadLine());
+Dictionary<int, string> map = new Dictionary<int, string>()
+{
+    {1, "Monday"},
+    {2, "Tuesday"},
+    {3, "Wednesday"},
+    {4, "Thursday"},
+    {5, "Friday"},
+    {6, "Saturday"},
+    {7, "Sunday"}
+};
+switch (n)
+{
+    case 1: Console.WriteLine($"{map[n]} рабочий день");
+        break;
+    case 2: Console.WriteLine($"{map[n]} рабочий день");
+        break;
+    case 3: Console.WriteLine($"{map[n]} рабочий день");
+        break;
+    case 4: Console.WriteLine($"{map[n]} рабочий день");
+        break;
+    case 5: Console.WriteLine($"{map[n]} рабочий день");
+        break;
+    case 6: Console.WriteLine($"{map[n]} выходной день");
+        break;
+    case 7: Console.WriteLine($"{map[n]} выходной день");
+        break;
+    
+}   
+*/
